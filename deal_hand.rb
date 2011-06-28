b59011b3e412c3c3ea3b4affbbc6ac8a39e2b93a
bobdutch <bobdutch@gmail.com>
2011-06-28 09:57:51 -0700

    class for Deck, moved constants into classes

--- deal_hand.rb
+++ deal_hand.rb
@@ -1,24 +1,20 @@
-ALLOWED_PLAYERS = 1..9
-
-SUITS = %w(d c s h)
-
-CARDS = (2..9).to_a + %w(T J Q K A)
-
-DECK = CARDS.product(SUITS).collect { |c| "#{c[0]}#{c[1]}"}
-
-
 class Game
+
+  ALLOWED_PLAYERS = 1..9
 
   def initialize(players, cards)
     #validate input
+
     #shuffle
-    @deck = DECK.sort_by { rand }
+    @deck = Deck.new
+    @deck.shuffle
+
     @players = Hash.new()
     #deal
     cards.times do
       (1..players).each do |i|
         @players[i] ||= Player.new
-        @players[i].hand << @deck.pop
+        @players[i].hand << @deck.draw_card
       end
     end
 
@@ -31,8 +27,29 @@ end
 
 class Player
   attr_accessor :hand
+
   def initialize
-    @hand = Array.new
+    self.hand = Array.new
+  end
+end
+
+class Deck
+  SUITS = %w(d c s h)
+
+  CLASSES = (2..9).to_a + %w(T J Q K A)
+
+  attr_accessor :cards
+
+  def initialize
+    self.cards = CLASSES.product(SUITS).collect { |c| "#{c[0]}#{c[1]}"}
+  end
+
+  def shuffle
+    self.cards = self.cards.sort_by { rand }
+  end
+
+  def draw_card
+    @cards.pop
   end
 end
 
