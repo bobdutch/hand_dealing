35725aeee6701bd8f7487c62a30fb78ddb11d6ad
bobdutch <bobdutch@gmail.com>
2011-06-28 08:43:51 -0700

    proof of concept app, procedural version

--- test.rb
+++ test.rb
@@ -6,22 +6,26 @@ CARDS = (2..9).to_a + %w(T J Q K A)
 
 DECK = CARDS.product(SUITS).collect { |c| "#{c[0]}#{c[1]}"}
 
-puts DECK
-
 
 class Game
 
   def initialize(players, cards)
     #validate input
     #shuffle
+    @deck = DECK.sort_by { rand }
+    @players = Hash.new()
     #deal
-    puts players
-  end
+    cards.times do
+      (1..players).each do |i|
+        @players[i] ||= Array.new
+        @players[i] << @deck.pop
+      end
+    end
 
-  def shuffle_deck
-  end
-
-  def deal
+    #print it out
+    @players.sort.each do |i,p|
+      puts "#{i}: #{p.join(' ')}"
+    end
   end
 end
 
