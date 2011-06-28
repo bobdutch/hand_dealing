e1b474280ed1725dcd85c980728c45164bc67b09
bobdutch <bobdutch@gmail.com>
2011-06-27 22:05:05 -0700

    setting up class

--- test.rb
+++ test.rb
@@ -8,3 +8,21 @@ DECK = CARDS.product(SUITS).collect { |c| "#{c[0]}#{c[1]}"}
 
 puts DECK
 
+
+class Game
+
+  def initialize(players, cards)
+    #validate input
+    #shuffle
+    #deal
+    puts players
+  end
+
+  def shuffle_deck
+  end
+
+  def deal
+  end
+end
+
+Game.new(6, 4)
