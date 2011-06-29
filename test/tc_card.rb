bf379e827a2d85ec51f9e09bfb5ae92ad786d080
bobdutch <bobdutch@gmail.com>
2011-06-28 22:04:14 -0700

    test that there are no setters for Card#suit and Card#rank

--- test/tc_card.rb
+++ test/tc_card.rb
@@ -6,6 +6,16 @@ class TC_Card < Test::Unit::TestCase
     @card = Card.new(Deck::SUITS[rand(Deck::SUITS.size)], Deck::RANKS[rand(Deck::RANKS.size)]) 
   end
 
+  #we shouldn't be able to change a card's suit or rank
+  def test_change_suit_or_rank
+    assert_raise(NoMethodError) do
+      @cart.suit = 's'
+    end
+    assert_raise(NoMethodError) do
+      @cart.rank = 's'
+    end
+  end
+
   def test_to_s
     rank, suit = @card.to_s.split('')
     assert_equal(suit, @card.suit)
