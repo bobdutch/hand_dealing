6d8d27c5911b58f091707f3bfeeaeeefe913c335
bobdutch <bobdutch@gmail.com>
2011-06-28 22:03:20 -0700

    make sure Card#to_s is formatting rank and suit as expected

--- test/tc_card.rb
+++ test/tc_card.rb
@@ -2,8 +2,13 @@ require 'test/unit'
 require "#{File.dirname(__FILE__)}/../lib/card.rb"
 
 class TC_Card < Test::Unit::TestCase
-  def test_true
-    assert_equal(true, true)
+  def setup
+    @card = Card.new(Deck::SUITS[rand(Deck::SUITS.size)], Deck::RANKS[rand(Deck::RANKS.size)]) 
   end
 
+  def test_to_s
+    rank, suit = @card.to_s.split('')
+    assert_equal(suit, @card.suit)
+    assert_equal(rank, @card.rank)
+  end
 end
