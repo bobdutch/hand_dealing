10f3e658a4d7ee8376e7c8ab3247c014224a89a0
bobdutch <bobdutch@gmail.com>
2011-06-30 08:55:00 -0700

    Tests for SeatList#current

--- test/tc_seat_list.rb
+++ test/tc_seat_list.rb
@@ -14,11 +14,34 @@ class TC_SeatList < Test::Unit::TestCase
     assert_equal(orig_count + 1, @list.count)
   end
 
-  def test_advance
-    flunk
+  def test_current_and_advance
+    #start with no players
+    assert_equal(@list.current, nil)
+
+    #add one and make sure he/she's current
+    @list.add_player(Player.new(1))
+    assert_instance_of(Player, @list.current)
+    first_current = @list.current
+
+    #advance a list of one shouldn't change current
+    assert_equal(@list.current, first_current)
+    @list.advance
+    assert_equal(@list.current, first_current)
+
+    #add another and advance
+    @list.add_player(Player.new(2))
+    assert_equal(@list.current, first_current)
+    @list.advance
+    assert_not_equal(@list.current, first_current)
+
+    #make sure it loops back to the first player
+    @list.advance
+    assert_equal(@list.current, first_current)
   end
 
+
   def test_count
+    assert_equal(0, @list.count)
     flunk
   end
 
