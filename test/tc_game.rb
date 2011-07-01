0af696d09c9cd0900a6c79ce79f398164bad89f0
bobdutch <bobdutch@gmail.com>
2011-07-01 08:24:24 -0700

    stubbed tests for Game class

---
+++ test/tc_game.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require "#{File.dirname(__FILE__)}/../lib/game.rb"
+require "#{File.dirname(__FILE__)}/../lib/seat_list.rb"
+require "#{File.dirname(__FILE__)}/../lib/player.rb"
+
+class TC_Game < Test::Unit::TestCase
+
+  def test_new
+    flunk
+  end
+
+  def test_deal
+    flunk
+  end
+
+end
+
