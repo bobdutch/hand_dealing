a50eac8997d57f05d1864cc6f0f8db4e48da26ab
bobdutch <bobdutch@gmail.com>
2011-06-30 21:45:26 -0700

    stubbed tests for Deck instance methods

--- test/tc_deck.rb
+++ test/tc_deck.rb
@@ -8,11 +8,19 @@ class TC_Deck < Test::Unit::TestCase
     @deck = Deck.new
   end
 
-  def test_draw
+  def test_draw_card
     start_length = @deck.cards.length
     @deck.draw_card
     assert_equal(start_length - 1, @deck.cards.length)
     assert_not_equal(start_length, @deck.cards.length)
+  end
+
+  def test_shuffle
+    flunk
+  end
+
+  def test_empty
+    flunk
   end
 end
 
