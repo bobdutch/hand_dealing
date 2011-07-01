e617866b9f0a6891b708aab88234ef70a2d22b03
bobdutch <bobdutch@gmail.com>
2011-07-01 08:19:33 -0700

    test for Deck#shuffle, will come back to it

--- test/tc_deck.rb
+++ test/tc_deck.rb
@@ -9,14 +9,19 @@ class TC_Deck < Test::Unit::TestCase
   end
 
   def test_draw_card
+    assert_not_equal(@deck.cards.length, 0)
     start_length = @deck.cards.length
     @deck.draw_card
     assert_equal(start_length - 1, @deck.cards.length)
     assert_not_equal(start_length, @deck.cards.length)
   end
 
+  #there is a 1/52 chance that a shuffled deck could be the same
+  #so this test is dubious
   def test_shuffle
-    flunk
+    pre_shuffle = @deck#.clone
+    @deck.shuffle
+    assert_not_equal(@deck, pre_shuffle, "these are the same?")
   end
 
   def test_empty
