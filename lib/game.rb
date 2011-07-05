64632b263113f6c1089c020212a67a6fd9868116
bobdutch <bobdutch@gmail.com>
2011-07-05 11:21:51 -0700

    Game#cards_per_hand and Game#num_players shouldn't have attr_writers

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,21 @@
+#An instance of Game sets up and controls the flow of the game
+#by acting as dealer, score keeper, etc.
 class Game
 
-  attr_accessor :num_players, :cards_per_hand
-  attr_reader :players, :deck 
+  attr_reader :players, :deck, :num_players, :cards_per_hand 
 
   #set up a new game
   def initialize(num_players, cards_per_hand)
-    self.num_players = num_players.to_i
-    self.cards_per_hand = cards_per_hand.to_i
+    @num_players = num_players.to_i
+    @cards_per_hand = cards_per_hand.to_i
 
     #validate
-    if self.num_players == 0
+    if @num_players == 0
       message = 'Number of players must be a positive number'
       raise NumPlayersError.new(message)
     end
 
-    if self.cards_per_hand == 0
+    if @cards_per_hand == 0
       message = 'Cards per hand must be a positive number'
       raise CardsPerHandError.new(message)
     end
@@ -23,15 +24,16 @@ class Game
 
     @players = SeatList.new
     @num_players.times do
-      player = Player.new
-      @players.add_player(player)
+      @players.add_player(Player.new
     end
 
     #shuffle
     @deck.shuffle!
     deal
   end
-
+  
+  #give cards to the players until their hands are full
+  #or the deck is empty
   def deal
     while (!@deck.empty? && @players.any? { |p| p.num_cards < self.cards_per_hand }) do
       @players.current.get_card(@deck.draw_card)
@@ -40,8 +42,12 @@ class Game
   end
 end
 
+#raise an error of this type if there is
+#invalid input for Game#num_players= 
 class NumPlayersError < ArgumentError
 end
 
+#raise an error of this type if there is
+#invalid input for Game#cards_per_hand= 
 class CardsPerHandError < ArgumentError
 end
