495ee2380b48bc249ddfb35d1ba6aaae2400284f
bobdutch <bobdutch@gmail.com>
2011-07-05 13:24:50 -0700

    method documentation for Card

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,12 @@
+#An instance of Card represents a standard playing card
+#in a 52-card deck.
 class Card
  SUITS = %w(d c s h)
  RANKS = (2..9).to_a + %w(T J Q K A)
 
  attr_reader :suit, :rank
 
+  #a new card: suit and rank are required parameters
   def initialize(suit, rank)
     unless SUITS.include?(suit) and RANKS.include?(rank)
       raise ArgumentError
@@ -12,6 +15,8 @@ class Card
     @rank = rank
   end
 
+  #string representation of card
+  #eg. a card with #rank J and #suit h would be "Jh"
   def to_s
     [self.rank,self.suit].join
   end
