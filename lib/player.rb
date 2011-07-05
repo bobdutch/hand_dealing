ffc0bdab21bb10a08be489d878223d21f5b61135
bobdutch <bobdutch@gmail.com>
2011-07-05 13:26:37 -0700

    method documentation for Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,19 @@
+#represents a person who is playing the game
+#the position method represents the seating order of the player
 class Player
   attr_accessor :hand, :position
 
+  #create a new person with an empty hand
   def initialize
     self.hand = Array.new
   end
 
+  #return the number of cards in the player's hand
   def num_cards
     self.hand.length
   end
 
+  #put the specified card in the player's hand
   def get_card(card)
     self.hand << card
   end
