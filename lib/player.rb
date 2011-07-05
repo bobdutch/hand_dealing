62d983b780c96dd66c981d473032b79e7ea96e1e
bobdutch <bobdutch@gmail.com>
2011-07-05 14:31:11 -0700

    Player#hand doesn't need an attr_writer

--- lib/player.rb
+++ lib/player.rb
@@ -1,25 +1,25 @@
 #represents a person who is playing the game
 class Player
-  attr_accessor :hand
+  attr_reader :hand
 
   #create a new person with an empty hand
   def initialize
-    self.hand = Array.new
+    @hand = Array.new
   end
 
   #return the number of cards in the player's hand
   def num_cards
-    self.hand.length
+    @hand.length
   end
 
   #put the specified card in the player's hand
   def take_card(card)
-    self.hand << card
+    @hand << card
   end
 
   #print out contents of the player's hand
   def to_s
-    self.hand.join(', ')
+    @hand.join(', ')
   end
 end
 
