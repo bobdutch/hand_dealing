c44489a4ed72b4abbf13f8eca10b82fe0f538181
bobdutch <bobdutch@gmail.com>
2011-07-05 13:25:59 -0700

    method documentation for Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,24 @@
+#a container class for Card objects
 class Deck
 
   attr_reader :cards
 
+  #build a full deck of cards
   def initialize
     @cards = Card::RANKS.product(Card::SUITS).collect { |rank, suit| Card.new(suit, rank) }
   end
 
+  #randomize the order of the cards in the deck
   def shuffle!
     @cards = self.cards.sort_by { rand }
   end
 
+  #return true if the deck has no cards
   def empty?
     self.cards.empty?
   end
 
+  #remove the card from the top of the deck and return it
   def draw_card
     self.cards.pop
   end
